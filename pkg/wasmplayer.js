03d3f546b8ccdceda1950b18436fbbcec70acb03
andros21 <andros21@users.noreply.github.com>
2025-10-05 07:12:07 +0000

    deploy: cbfe2291d25e5731c7cf2d9ddae255f9b4b1203b

--- pkg/wasmplayer.js
+++ pkg/wasmplayer.js
@@ -203,7 +203,7 @@ export function main() {
 }
 
 function __wbg_adapter_18(arg0, arg1) {
-    wasm._dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h80622fa28cb829a7(arg0, arg1);
+    wasm._dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h91a7d6d5d1966b32(arg0, arg1);
 }
 
 function __wbg_adapter_21(arg0, arg1, arg2) {
